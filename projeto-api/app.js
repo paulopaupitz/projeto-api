bbddb8a3d4c8efd694ce6c200cd611df0c7b212b
Paulo Paupitz Neto <paupitzneto@gmail.com>
2024-12-02 04:36:39 -0300

    Autenticação e acesso restrito em rotas
    Tentativa de negar acesso não autorizado para rotas que não possuem token

--- projeto-api/app.js
+++ projeto-api/app.js
@@ -3,15 +3,20 @@ const userRoutes = require('./routes/userRoutes');
 const petRoutes = require('./routes/petRoutes');
 const donoRoutes = require('./routes/donoRoutes');
 const restrictAccessMiddleware = require('./middlewares/restrictAccessMiddleware');
+const validateDataMiddleware = require('./middlewares/validateDataMiddleware');
+const errorHandlerMiddleware = require('./middlewares/errorHandlerMiddleware');
 
 const app = express();
 app.use(express.json());
+app.use(validateDataMiddleware);
 
-app.use('/pets', petRoutes);
-app.use('/donos', donoRoutes);
+app.use('/pets', restrictAccessMiddleware, petRoutes);
+app.use('/donos', restrictAccessMiddleware, donoRoutes);
 app.use('/users', restrictAccessMiddleware, userRoutes);
 
+app.use(errorHandlerMiddleware);
 
+// Inicialização do servidor
 app.listen(3000, () => {
-  console.log(`Servidor ouvindo a porta 3000`);
+  console.log('Servidor ouvindo a porta 3000');
 });
